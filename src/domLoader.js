11eabe07abd2d1793288c1c369ac9ea6ba499ef4
Einschleicher <116537717+Einschleicher@users.noreply.github.com>
2023-09-01 12:20:14 +0200

    Add highlighting for active project

--- src/domLoader.js
+++ src/domLoader.js
@@ -8,18 +8,26 @@ const sideMenu = document.querySelector(".side-menu");
 // }
 
 function renderPage() {
-    renderProjects();
+    renderSidebar();
+    renderMain();
     // addListeners();
     console.log("Page rendered...");
 }
 
-function renderProjects() {
+function renderSidebar() {
     projects.forEach(project => {
         const projectDiv = document.createElement("div");
-        projectDiv.classList = "project";
+        projectDiv.className = "project";
+        // projectDiv.classList.add("project");
+        // console.log(project.active)
+        if (project.active) projectDiv.classList.add("active");
         sideMenu.appendChild(projectDiv);
         projectDiv.innerText = project.name;
     });
+}
+
+function renderMain() {
+
 }
 
 function addProject() {
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { renderPage } from "./domLoader";
 const projects = [];
 
 projects.push(projectFactory("Work", "Well, I just like to work"));
+projects[0].active = true;
+
 projects.push(projectFactory("Hobbies", "I do have hobbies though"));
 projects.push(projectFactory("Car", "Always broken"));
 
