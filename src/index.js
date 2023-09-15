82f98a1ee97effd21eeeb0cb7de5665975cc5e78
Einschleicher <116537717+Einschleicher@users.noreply.github.com>
2023-09-15 21:41:04 +0200

    Clean up default projects and todos

--- src/index.js
+++ src/index.js
@@ -4,13 +4,9 @@ import { renderPage, getStaticEventListeners } from "./domLoader";
 // Create default project
 const projects = [];
 
-projects.push(projectFactory("Work", "Well, I just like to work"));
+projects.push(projectFactory("Default Project", "Well, this is just a template"));
 projects[0].active = true;
-projects[0].todos.push(todoFactory("Default Todo", "Enter some description here", "2023-09-02", null));
-projects[0].todos.push(todoFactory("Another Default One", "I love Hackerhouse Berlin", "2023-09-02", null));
-
-projects.push(projectFactory("Hobbies", "I do have hobbies though"));
-projects.push(projectFactory("Car", "Always broken"));
+projects[0].todos.push(todoFactory("Default Todo", "Enter some description here", "2023-09-15", null));
 
 renderPage();
 getStaticEventListeners();
